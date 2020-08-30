8de16d59f4146e6ac66f6cda18fd6f2e15f4fe42
Nok <dive877@gmail.com>
2020-08-30 14:15:49 +0400

    corrected typo in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LazyLoad from 'react-lazyload';
-import MainPage from './components/mainPage';
+import MainPage from './components/MainPage';
 import StatPage from './components/StatPage';
 import DescPage from './components/DescPage';
 const App = () => {
