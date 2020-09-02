763dc6472bb335125a980d2f6b1000c2f0dc8875
Nok <dive877@gmail.com>
2020-09-02 15:40:34 +0400

    Improve responsivness

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,9 @@ import React from 'react';
 import documentation from '../images/documentation.png';
 import weather from '../images/weather.png';
 import unsplash from '../images/unsplash.png';
-import html from '../images/css-logo.png';
+import html from '../images/html.png';
+import css from '../images/css.png';
+import npm from '../images/npm.png';
 import js from '../images/js.png';
 import rlogo from '../images/react-logo.png';
 import parc from '../images/parcel.svg';
@@ -12,67 +14,146 @@ const Portfolio = () => {
       <h1>Portfolio</h1>
       <div className="portfolio__container">
         <div className="portfolio__container--project">
-          <a href="https://codepen.io/Nokrei/full/rXygyq" target="_blank">
-            {' '}
-            <img
-              className="portfolio__container--project--image"
-              src={documentation}
-            />
-          </a>
-          <p>Documentation page</p>
-          <br />
-          <div className="tech">
-            <p>Technologies used:</p>{' '}
-            <div className="tech__images">
-              <img className="portfolio__container--project--tech" src={html} />
-            </div>
-        
-          </div>
-        </div>
-        <div className="portfolio__container--project">
-          <a href="https://focused-colden-1baa12.netlify.app/" target="_blank">
-            <img
-              className="portfolio__container--project--image"
-              src={weather}
-            />
-          </a>
-          <p>Weather App</p>
-          <br/>
-          <div className="tech">
-            <p>Technologies used:</p>
-            <div className="tech__images">
-              <img className="portfolio__container--project--tech" src={html} />
-              <img className="portfolio__container--project--tech" src={js} />
+          <div className="project">
+            <a href="https://codepen.io/Nokrei/full/rXygyq" target="_blank">
+              {' '}
               <img
-                className="portfolio__container--project--tech"
-                src={rlogo}
+                className="portfolio__container--project--image"
+                src={documentation}
               />
-              <img className="portfolio__container--project--tech" src={parc} />
+            </a>
+          </div>
+          <div className="tech">
+            <div className="tech__desc">
+              <h2>Documentation page</h2>
+              <br />
+              <p>
+              A simple documentation page for JavaScript.
+              </p>
+              
+            </div>
+            <div className="tech__content">
+              <h2>Stack:</h2> <br />
+              <div className="tech__images">
+                <img
+                  className="portfolio__container--project--tech"
+                  src={html}
+                />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={css}
+                />
+              </div>
             </div>
           </div>
         </div>
         <div className="portfolio__container--project">
-          <a
-            href="https://compassionate-lovelace-ca8460.netlify.app/"
-            target="_blank"
-          >
-            <img
-              className="portfolio__container--project--image"
-              src={unsplash}
-            />
-          </a>
-          <p>Image finder App</p>
-          <br/>
-          <div className="tech">
-            <p>Technologies used:</p>
-            <div className="tech__images">
-              <img className="portfolio__container--project--tech" src={html} />
-              <img className="portfolio__container--project--tech" src={js} />
+          <div className="project">
+            <a
+              href="https://focused-colden-1baa12.netlify.app/"
+              target="_blank"
+            >
               <img
-                className="portfolio__container--project--tech"
-                src={rlogo}
+                className="portfolio__container--project--image"
+                src={weather}
               />
-              <img className="portfolio__container--project--tech" src={parc} />
+            </a>
+          </div>
+
+          <div className="tech">
+            <div className="tech__desc">
+              <h2>Weather App</h2>
+              <br />
+              <p>
+              A weather web application. Initially it uses the locationiq API
+                to detect users position and display the weather for that
+                location. It also uses the openweather API to display the
+                weather forecast for any location searched by the user. Number
+                of searches limited due to using free variants of the APIs.
+              </p>
+               
+              
+            </div>
+            <div className="tech__content">
+              <h2>Stack:</h2>
+              <br />
+
+              <div className="tech__images">
+                <img
+                  className="portfolio__container--project--tech"
+                  src={html}
+                />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={css}
+                />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={npm}
+                />
+                <img className="portfolio__container--project--tech" src={js} />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={rlogo}
+                />
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio__container--project">
+          <div className="project">
+            <a
+              href="https://compassionate-lovelace-ca8460.netlify.app/"
+              target="_blank"
+            >
+              <img
+                className="portfolio__container--project--image"
+                src={unsplash}
+              />
+            </a>
+          </div>
+          <div className="tech">
+            <div className="tech__desc">
+              <h2>Image search APP</h2>
+              <br />
+              <p>
+              An image search web application. It uses the Unsplash API to
+                display imagaes searched by the user. Infinite scrolling is
+                acheved via the implementation of a custom React hook, the
+                layout and loading-in animation by the use of the StackGrid
+                library. Number of searches limited due to using the free
+                variant of the Unspash API.
+              </p>
+                
+              
+            </div>
+
+            
+            <div className="tech__content">
+              <h2>Stack:</h2>
+              <br />
+
+              <div className="tech__images">
+                <img
+                  className="portfolio__container--project--tech"
+                  src={html}
+                />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={css}
+                />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={npm}
+                />
+                <img className="portfolio__container--project--tech" src={js} />
+                <img
+                  className="portfolio__container--project--tech"
+                  src={rlogo}
+                />
+                
+              </div>
             </div>
           </div>
         </div>
