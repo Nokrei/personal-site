fabb13cb85dab251ee689d08fd7c4f09b8fff750
Nok <dive877@gmail.com>
2020-12-17 15:33:31 +0400

    Shorten About section, add links and animation to About section, adjust CSS

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,28 +6,36 @@ const AboutPage = () => {
     <div className="aboutPage" id="aboutPage">
       <div className="aboutPage__container">
         <h1>About me</h1>
-        
-        <br/>
+
+        <br />
         <br />
 
         <div className="aboutPage__content">
           <p>
             I am a web developer focusing mainly on the front-end, however I
             have the ability to deliver full stack applications as well. <br />
-            <br /> My tech stack is HTML5, CSS3, JavaScript ES6+, ReactJS,
-            NodeJS(Express), MongoDB(Mongoose). I have experience using Version
-            Control Systems(GIT) and Package Managers(NPM). <br /> <br />
-            In my projects I practice the principles of Responsive Web Design,
-            Mobile First Design and I follow the BEM methodology. <br /> <br />A
-            large part of my professional career revolved around IT, but it is
+            <br />
+            A large part of my professional career revolved around IT, but it is
             in web development where I found my true passion and calling. <br />
             <br />
-            Recently I have completed the Astrolabs Full-Stack Developer
-            Bootcamp, which greatly supplemented my existing skills and
-            knowledge. I am extremely passionate about my work and I always want
-            to learn more. <br />
-            <br />I am open to both freelance opportunities and full-time
-            positions.
+            Have a project you would like to discuss? Feel free to contact me on
+            &nbsp;
+            <a
+              className="animateUnderline"
+              href="https://www.linkedin.com/in/piotr-mrozowski-26ab03198/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            ,{' '}
+            <a className="animateUnderline" href="mailto:pmrozowski87@gmail.com">
+              email me
+            </a>{' '}
+            or use the{' '}
+            <a className="animateUnderline" href='#contact'>
+              Conact form
+            </a>{' '}
+            bellow.
           </p>
           <img className="personalPic" src={personal} />
         </div>
