5666320e50fa0d4457673691347594ec107065c2
Nok <dive877@gmail.com>
2020-12-21 18:09:05 +0400

    ternary operator was't the problem, switching back to conditional rendering

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,58 +5,60 @@ import useWindowDimensions from './useWindowDimensions';
 const MainPage = () => {
   const { width } = useWindowDimensions();
   return (
-    <div className="mainPage" id="mainPage">
+    <div className="mainPage" id='mainPage'>
+      {width > 1025 && 
+         <Particles
+         style={{ position: 'absolute' }}
+         params={{
+           particles: {
+             number: {
+               value: 100,
+               density: {
+                 enable: true,
+                 value_area: 1000,
+               },
+             },
+           },
+           interactivity: {
+             detectsOn: 'window',
+             events: {
+               onhover: {
+                 enable: true,
+                 mode: 'grab',
+               },
+               onclick: {
+                 enable: true,
+                 mode: 'push',
+               },
+               resize: true,
+             },
+           },
+           move: {
+             enable: true,
+             speed: 6,
+             direction: 'none',
+             random: false,
+             straight: false,
+             out_mode: 'out',
+             bounce: false,
+             attract: {
+               enable: false,
+               rotateX: 600,
+               rotateY: 1200,
+             },
+           },
+         }}
+       />
+      }
      
-        <Particles
-          style={{ position: 'absolute' }}
-          params={{
-            particles: {
-              number: {
-                value: 100,
-                density: {
-                  enable: true,
-                  value_area: 1000,
-                },
-              },
-            },
-            interactivity: {
-              detectsOn: 'window',
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'grab',
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'push',
-                },
-                resize: true,
-              },
-            },
-            move: {
-              enable: true,
-              speed: 6,
-              direction: 'none',
-              random: false,
-              straight: false,
-              out_mode: 'out',
-              bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          }}
-        />
-      ) 
 
-      <Header />
-
+      <Header/>
+       
       <div className="mainPage--content">
         <span>Web Development</span> <br />
         <span>With</span> <br /> <span>a Passion</span>
       </div>
+      
     </div>
   );
 };
